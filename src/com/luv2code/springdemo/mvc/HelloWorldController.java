8e60ab22587e5f38a4d77be4ccb2218674024c4a
Holly Williams <williamsh@objectcomputing.com>
2019-04-09 10:56:47 -0500

    section 13 request params and request mappings

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	@RequestMapping("/showForm")
@@ -35,6 +37,20 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		theName = theName.toUpperCase();
+		String result = "Hey my friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		// return the helloworld.jsp
+		return "helloworld";
+	}
 	
 	
 	
